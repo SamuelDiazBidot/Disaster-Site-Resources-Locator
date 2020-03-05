ab5a7660ea5bd85d7a8e5e94de61cc1fb3cc24fb
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2020-03-05 15:44:07 -0400

    Fixed return dummy data

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 from flask import Flask, jsonify, request 
 
-resources = [
+resourcesRequested = [
     { "id" : 0
     , "type" : "water bottle"
     , "name" : "Nikini bottles"
-    , "description": "36 water bottle pack"
+    , "description" : "36 water bottle pack"
+    , "requesterName" : "Johnny Sins"
+    , "location" : "NA"
     },
     { "id" : 1
     , "type" : "baby food"
     , "name" : "gerber baby food"
     , "description": "bannana flavor"
+    , "requesterName" : "Jose Rivera"
+    , "location" : "Ponce, PR"
     },
     { "id" : 2
     , "type" : "gasoline"
     , "name": "puma gas"
     , "description": "20 liters"
+    , "requesterName" : "Miguel Navarro"
+    , "location" : "San Juan, PR"
+    }
+]
+
+resourcesAvailable = [
+    { "id" : 0
+    , "type" : "water bottle"
+    , "name" : "Nikini bottles"
+    , "description" : "36 water bottle pack"
+    , "SupplierName" : "Johnny Sins"
+    , "location" : "NA"
+    , "price" : 0.0
+    },
+    { "id" : 1
+    , "type" : "baby food"
+    , "name" : "gerber baby food"
+    , "description": "bannana flavor"
+    , "SupplierName" : "Jose Rivera"
+    , "location" : "Ponce, PR"
+    , "price" : 0.0 
+    },
+    { "id" : 2
+    , "type" : "gasoline"
+    , "name": "puma gas"
+    , "description": "20 liters"
+    , "supplierName" : "Miguel Navarro"
+    , "location" : "San Juan, PR"
+    , "price" : 10.0
     }
 ]
 
@@ -25,15 +58,14 @@ def greetings():
     return "Welcome to the Disaster Site Resource Locator"
 
 # Route to see and post requests for resources
-# Le falta la info del que esta requesting (nombre, localizacion, id)
 @app.route('/resources/request', methods=['GET', 'POST'])
 def requested():
     # Return the resource request and a 201 Ok response if valid
     if request.method == 'POST':
-        return jsonify(Request = resources[0]), 201
+        return jsonify(Request = resourcesRequested[0]), 201
     # Return all requested resources
     else:
-        return jsonify(Requests = resources)
+        return jsonify(Requests = resourcesRequested)
 
 # Route to see and post available resources
 # Le falta la info de quien esta supliendo y si es una donacion o no
@@ -41,19 +73,19 @@ def requested():
 def available():
     # Return the resource suplied and a 201 Ok response if valid
     if request.method == 'POST':
-        return jsonify(Available = resources[0]), 201
+        return jsonify(Available = resourcesAvailable[0]), 201
     # Return all available resources
     else:
-        return jsonify(Available = resources)
+        return jsonify(Available = resourcesAvailable)
 
 # Route to see the available resource with a specific id
 @app.route('/resources/available/<int:id>', methods=['GET', 'PUT'])
 def availableByID(id):
     # GET info of a specific available resource
     if request.method == 'GET':
-        return jsonify(available = resources[0]), 201
+        return jsonify(available = resourcesAvailable[0]), 201
     # Reserve or purchase a resource if its not already taken
     elif request.method == 'PUT':
-        return jsonify(available = resources[0]), 201
+        return jsonify(available = resourcesAvailable[0]), 201
 
 
